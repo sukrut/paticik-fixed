ebb5949a555702cc2971b1db6b1261877069ae36
Hüseyin Uslu <shalafiraistlin@gmail.com>
2019-03-12 22:50:23 +0300

    revert back version

--- fixed.user.js
+++ fixed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         paticik-fixed-debug
 // @include      https://forum.paticik.com/*
-// @version      0.4
+// @version      0.3
 // @description  green theme fixed
 // @author       bonesoul
 // @run-at       document-start
